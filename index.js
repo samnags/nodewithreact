1de7612f7df30493e6c0ec7a8ba92a90e768e96f
samnags <sam.nagourney@gmail.com>
2018-01-07 14:16:30 -0800

    Initial sever setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, rhees) => {
+    res.send({ hi: 'there'})
+})
+
+const PORT = process.env.port || 5000
+// Heroku will set env vironments that they'll pass us on runtime.
+
+app.listen(PORT)
+console.log('Running on port, PORT')
