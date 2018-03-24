26844e1871222454e1e9a2b7464cb971f0da9414
samnags <sam.nagourney@gmail.com>
2018-03-24 10:47:24 -1000

    Update package.json to handle heroku build with dev dependencies

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 // authRoutes is exporting an a function. Here we're executing that function with the argument of app
 
+if (process.env.env == 'production') {
+  // Make sure Express serves production assets like main.js
+  // Searching for specific files first then moves onto below
+  app.use(express.static('/client/build'));
+
+  // Makes sure that Express servers index.js /react routes if it doesn't recognize route
+  // If there's nothing in authRouthes, billRouthes, look above, then hit below catchall
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 // Heroku will set env vironments that they'll pass us on runtime.
 
