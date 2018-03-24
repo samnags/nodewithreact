58815982f12d6a9cd982f5f4d45a43553ad2be3c
samnags <sam.nagourney@gmail.com>
2018-03-23 18:09:07 -1000

    billing post request setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 // Takes cookie and adds a session key to the request object
 
 const passport = require('passport');
@@ -14,6 +15,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -26,6 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 // authRoutes is exporting an a function. Here we're executing that function with the argument of app
 
 const PORT = process.env.PORT || 5000;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -29,6 +29,7 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
+    res.send(req.user);
     res.redirect('/');
   });
 };
